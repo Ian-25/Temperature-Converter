f7fefc20ba67aa726a039400370e7bd16b1532b9
Ian-25 <130345983+Ian-25@users.noreply.github.com>
2024-10-01 16:42:49 +0800

    Add files via upload

---
+++ Word Reverser/WordReverser.js
@@ -0,0 +1,7 @@
+function reverseWord(word) {
+    return word.split('').reverse().join('');
+}
+
+console.log(reverseWord("JavaScript")); 
+console.log(reverseWord("OpenAI"));
+console.log(reverseWord("racecar"));
